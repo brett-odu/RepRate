08d48c5960ebe88e0e5e334fe6647d5b43e59cd8
brett-odu <bwarr011@odu.edu>
2021-11-08 10:32:20 -0500

    Create Greeting package Create Hello.py

---
+++ Greeting/Hello.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Hello Gold Team! If you would like to test PyCharm with Github, make a change to this function's argument,
+    # commit the change, then push.
+    print_hi('Brett was here.')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
